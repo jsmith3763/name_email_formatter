b23d9f39e6ad103b1614d1ed393d00a505b35ee3
jsmith3763 <jsmith3763@gmail.com>
2023-01-04 13:27:47 -0800

    Finished

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if(args.length == 1) {
+            System.out.println("Please pass an email for " + args[0]);
+            System.exit(1);
+        }else if(args.length !=2) {
+            System.out.println("Please specify a name and email");
+            System.exit(1);
+        }
+
+        System.out.println(args[0] + " <" + args[1] + ">");
     }
 }
